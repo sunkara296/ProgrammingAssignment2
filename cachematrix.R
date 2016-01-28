081e44308523aa8f85bc8857793a3ffde3634012
GayanWithanage <gayan.parakrama@gmail.com>
2016-01-28 19:07:01 +0530

    Update cachematrix.R
    The 'makeCacheMatrix'  creates a special "matrix" object while 'cacheSolve' function computes the inverse of the special "matrix" returned.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## 'makeCacheMatrix'  creates a special "matrix" object while 'cacheSolve' function computes the inverse of the special "matrix" returned.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## 'makeCacheMatrix'  creates a special "matrix" object that can cache its inverse.
+makeCacheMatrix <- function(dig = matrix()) {
+  int <- NULL
+  set <- function(a) {
+    dig <<- a
+    int <<- NULL
+  }
+  get <- function() dig
+  setinverse <- function(inverse) int <<- inverse
+  getinverse <- function() int
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## 'cacheSolve' function computes the inverse of the special "matrix" returned by makeCacheMatrix above and 
+## if the inverse has already been calculated (and the matrix has not changed), 
+## then cacheSolve retrieve the inverse from the cache.
+cacheSolve <- function(dig, ...) {
+  int <- int$getinverse()   ## Return a matrix that is the inverse of 'dig'
+  if(!is.null(int)) {
+    message("getting cached data")
+    return(int)
+  }
+  else{
+    data <- dig$get()
+    int <- inverse(data, ...)
+    dig$setinverse(int)
+    int
+  }
 }
